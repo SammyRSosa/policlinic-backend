be6eee383ae40cc7390631efe3af4f895face3e8
ChickenLittle02 <rubnmartinezrojas@gmail.com>
2025-12-15 08:48:00 -0500

    cambios en restricciones med-order-item y test de consultation

--- src/medication-order-items/medication-order-item.entity.ts
+++ src/medication-order-items/medication-order-item.entity.ts
@@ -1,8 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne ,Unique, } from 'typeorm';
 import { MedicationOrder } from 'src/medication-orders/medication-order.entity';
 import { Medication } from 'src/medications/medication.entity';
 
 @Entity('medication_order_items')
+@Unique(['medicationOrder', 'medication'])
 export class MedicationOrderItem {
   @PrimaryGeneratedColumn('uuid')
   id: string;
